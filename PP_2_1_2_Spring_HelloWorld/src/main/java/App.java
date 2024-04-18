e8aa9447f3066adf4f0158702e683f80966c2f0f
Danil <daniltikhonov@gmail.con>
2024-04-18 18:24:24 +0300

    правка

--- PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
+++ PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -5,12 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
-        Cat beanCat =
-                (Cat) applicationContext.getBean("cat");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean==bean1);
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
         System.out.println(beanCat.getName());
-
+        Cat bencat1=(Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat==bencat1);
     }
 }
